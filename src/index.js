e17f65f8f053feedfd4b88eb7b6e050791ee0861
Floyd Price <f.price@floydprice.com>
2017-09-12 15:03:55 +0100

    local env switch to decide when to listin on the https server (or not)

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@
 
 const assert = require('assert')
 const Provider = require('oidc-provider')
-const https = require('https')
+
 const fs = require('fs')
 const express = require('express')
 
@@ -19,7 +19,6 @@ const options = {
 
 
 // TODO : Work out the URL based on the ENV...
-console.log(`https://${process.env.HOST}:${process.env.PORT}`)
 const oidc = new Provider(`https://${process.env.HOST}:${process.env.PORT}`, {
     features: {
         claimsParameter: true,
@@ -44,12 +43,20 @@ oidc.initialize({
     app.proxy = true
     app.keys = process.env.SECURE_KEY.split(',')
     app.use(oidc.callback)
-    const port = process.env.PORT
-    const server = https.createServer(options, app)
 
-    server.listen(port, function () {
-        console.log('Express server listening on port ' + server.address().port)
-    })
+    const port = process.env.PORT
+
+    if (process.env.NODE_ENV === undefined || process.env.NODE_ENV === 'dev') {
+        const https = require('https')
+        const server = https.createServer(options, app)
+
+        server.listen(port, function () {
+            console.log(`Dev server listening on https://${process.env.HOST}:${process.env.PORT}`)
+        })
+    } else {
+        app.listen(port)
+    }
+
 }).catch((e) => {
         console.log(e)
     }
