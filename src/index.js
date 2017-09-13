a5e0a5d8fab2c95e079567aec93307d3406ea432
Dan Ashton <daniel.ashton@dashkatconsulting.com>
2017-09-13 15:26:05 +0100

    Added the interactions in the index for testing

--- src/index.js
+++ src/index.js
@@ -1,66 +1,103 @@
-'use strict'
+'use strict';
 
 // see previous example for the things that are not commented
 
-const assert = require('assert')
-const Provider = require('oidc-provider')
+const assert = require('assert');
+const Provider = require('oidc-provider');
 
-const fs = require('fs')
-const express = require('express')
+const fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
 const adapter = require('./adapters/MasterAdapter');
 
 const options = {
-    key: fs.readFileSync('./ssl/localhost.key'),
-    cert: fs.readFileSync('./ssl/localhost.cert'),
-    requestCert: false,
-    rejectUnauthorized: false
-}
+  key: fs.readFileSync('./ssl/localhost.key'),
+  cert: fs.readFileSync('./ssl/localhost.cert'),
+  requestCert: false,
+  rejectUnauthorized: false,
+};
 
 
 // TODO : Work out the URL based on the ENV...
 const oidc = new Provider(`https://${process.env.HOST}:${process.env.PORT}`, {
-    clientCacheDuration: 60,
-    features: {
-        claimsParameter: true,
-        discovery: true,
-        encryption: true,
-        introspection: true,
-        registration: true,
-        request: true,
-        revocation: true,
-        sessionManagement: true
-    }
-})
+  clientCacheDuration: 60,
+  features: {
+    claimsParameter: true,
+    discovery: true,
+    encryption: true,
+    introspection: true,
+    registration: true,
+    request: true,
+    revocation: true,
+    sessionManagement: true,
+  },
+});
 
-//TODO : Work out a better way of managing Keys when not in Dev...
-const keystore = require('./keystore.json')
+// TODO : Work out a better way of managing Keys when not in Dev...
+const keystore = require('./keystore.json');
 
 oidc.initialize({
 
-    keystore,
-    adapter
+  keystore,
+  adapter,
 }).then(() => {
-    app.proxy = true
-    app.keys = process.env.SECURE_KEY.split(',')
-    app.use(oidc.callback)
+  app.proxy = true;
+  app.keys = process.env.SECURE_KEY.split(',');
+  app.use(oidc.callback);
 
-    const port = process.env.PORT
+  const port = process.env.PORT;
 
-    if (process.env.NODE_ENV === undefined || process.env.NODE_ENV === 'dev') {
-        const https = require('https')
-        const server = https.createServer(options, app)
+  if (process.env.NODE_ENV === undefined || process.env.NODE_ENV === 'dev') {
+    const https = require('https');
+    const server = https.createServer(options, app);
 
-        server.listen(port, function () {
-            console.log(`Dev server listening on https://${process.env.HOST}:${process.env.PORT}`)
-        })
-    } else {
-        app.listen(port)
-    }
+    server.listen(port, () => {
+      console.log(`Dev server listening on https://${process.env.HOST}:${process.env.PORT}`);
+    });
+  } else {
+    app.listen(port);
+  }
+
+  app.get('/interaction/:grant', async (ctx, next) => {
+    oidc.interactionDetails(next).then((details) => {
+      console.log('see what else is available to you for interaction views', details);
+
+      // TODO To come from config
+      const url = `https://floating-temple-42985.herokuapp.com/usernamepassword?uuid=${details.uuid}`;
+      next.redirect(url);
+    });
+  });
+
+  const body = bodyParser();
+
+  app.post('/interaction/:grant/confirm', body, async (ctx, next) => {
+    const result = { consent: {} };
+    await oidc.interactionFinished(ctx.req, ctx.res, result);
+    await next();
+  });
+
+  app.post('/interaction/:grant/login', body, async (ctx, next) => {
+    const account = await Account.findByLogin(ctx.request.body.login);
+
+    const result = {
+      login: {
+        account: account.accountId,
+        acr: 'urn:mace:incommon:iap:bronze',
+        amr: ['pwd'],
+        remember: !!ctx.request.body.remember,
+        ts: Math.floor(Date.now() / 1000),
+      },
+      consent: {},
+    };
+
+    await oidc.interactionFinished(ctx.req, ctx.res, result);
+    await next();
+  });
 
 }).catch((e) => {
-        console.log(e)
-    }
-)
+  console.log(e);
+},
+);
