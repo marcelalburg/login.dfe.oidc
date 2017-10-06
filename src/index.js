989660163e633a547447c8392ca5ee582601a52e
Floyd Price <f.price@floydprice.com>
2017-10-06 17:11:55 +0100

    clientid is now passed to the interactions

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ const oidc = new Provider(`${config.hostingEnvironment.protocol}://${config.host
     email: ['email'],
   },
   interactionUrl(ctx) {
-    return `/interaction/${ctx.oidc.uuid}?clientid=${ctx.oidc.client.clientId}`;
+    return `/interaction/${ctx.oidc.uuid}`;
   },
   // TODO deployment configuration
   features: {
