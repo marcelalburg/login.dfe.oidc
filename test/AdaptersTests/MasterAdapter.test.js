71db45264f7ee1a47bb7de9263dca8561174add4
Simon Bull <simon.bull.84@googlemail.com>
2017-10-27 11:33:39 +0100

    Fix test

--- test/AdaptersTests/MasterAdapter.test.js
+++ test/AdaptersTests/MasterAdapter.test.js
@@ -2,13 +2,14 @@ const expect = require('chai').expect;
 
 const MasterAdapter = require('../../src/adapters/MasterAdapter');
 const GenericAdapter = require('../../src/adapters/GenericAdapter');
+const RedisAdapter = require('../../src/adapters/RedisAdapter');
 const HotConfigAdapter = require('../../src/HotConfig/HotConfigAdapter');
 
 describe('When constructing the adapter', () => {
   describe('then if no override is defined', () => {
-    it('the GenericAdapter is used', () => {
+    it('the RedisAdapter is used', () => {
       const actual = new MasterAdapter('test');
-      expect(actual.innerAdapter).to.be.an.instanceOf(GenericAdapter);
+      expect(actual.innerAdapter).to.be.an.instanceOf(RedisAdapter);
     });
   });
   describe('then if Client is defined as the constructor name', () => {
