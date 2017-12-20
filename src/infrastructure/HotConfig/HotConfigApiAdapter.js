f8eb671014a4885bab496b2a61789639ae0f7f81
Daniel Ashton <daniel.ashton@dashkatconsulting.com>
2017-12-20 10:17:14 +0000

    Update hot config api to use CTX
    Added the correlationId to the request

--- src/infrastructure/HotConfig/HotConfigApiAdapter.js
+++ src/infrastructure/HotConfig/HotConfigApiAdapter.js
@@ -4,15 +4,24 @@ const HotConfigAdapter = require('./HotConfigAdapter');
 const request = require('request-promise');
 const config = require('./../Config');
 const jwtStrategy = require('login.dfe.jwt-strategies');
+const uuid = require('uuid/v4');
 
 class HotConfigApiAdapter extends HotConfigAdapter {
-  async find(id) {
+  async find(id, ctx) {
     const bearerToken = await jwtStrategy(config.hotConfig).getBearerToken();
+    let correlationId = uuid();
+    if (ctx && ctx.req && ctx.req.id) {
+      correlationId = ctx.req.id;
+    }
+
     try {
       const response = await request.get(config.hotConfig.url, {
         auth: { bearer: bearerToken },
         strictSSL: false,
         resolveWithFullResponse: true,
+        headers: {
+          'x-correlation-id': correlationId,
+        },
       });
       let returnValue = null;
       if (response.statusCode === 200) {
