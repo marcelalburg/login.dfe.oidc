0e00db0f10e45a5d456982bca7e46e7baea384d9
Simon Bull <simon.bull.84@googlemail.com>
2018-03-27 13:11:11 +0100

    Use congif app name where available

--- src/infrastructure/logger/index.js
+++ src/infrastructure/logger/index.js
@@ -49,7 +49,7 @@ if (config.hostingEnvironment.applicationInsights) {
   appInsights.setup(config.hostingEnvironment.applicationInsights).setAutoCollectConsole(false, false).start();
   loggerConfig.transports.push(new AppInsightsTransport({
     client: appInsights.defaultClient,
-    applicationName: 'OIDC',
+    applicationName: config.loggerSettings.applicationName || 'OIDC',
     type: 'event',
     treatErrorsAsExceptions: true,
   }));
